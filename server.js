231aa6d9c79e7d23a47732a99577a95cbb087c34
Victor <vincitore.code@gmail.com>
2025-06-27 15:10:20 -0600

    controller

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ const allowedOrigins = [
   'https://innovatube-frontend.vercel.app'
 ];
 
+
+app.use(express.json());
 app.use(cors({
   origin: function(origin, callback) {
     if (!origin) return callback(null, true);
@@ -30,7 +32,7 @@ app.use(cors({
   }
 }));
 
-app.use(express.json());
+
 app.use(morgan('dev'));
 
 // Rutas
